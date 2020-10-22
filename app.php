07348197ab97fcb67cfca957ddcd945a38d72633
Wanjiku Maina <mainamary321@gmail.com>
2020-10-22 16:45:33 +0300

    gamification

---
+++ app.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="leaderboard.css">
+   
+
+
+</head>
+
+<body>
+
+
+    <div class=" jumbotron align-items-center container pt-10 main-container" >
+        <h2 class='main-header page-header' style="text-align: center; font-size: 2.3em; color: #000;">Gamification</h2>
+        <div class="row font-weight-bold py-4 align-items-center">
+            <div class="col col-sm-4 align-items-center mx-0 justify-content-center text-center my-col">
+                <div class="row  center align-items-center mx-0  my-row justify-content-center"
+                    style="display: flex; justify-content:center">
+                    <div class="div-img container" style="display: inline-block;">
+                    <div class="container">
+                      <div class="container" style="position:relative;">
+                      <img class="img-responsive img-fluid" src="images/badge__icon.jpeg" style="display:inline-block;
+                      position: relative; max-width:200px; max-height:auto;">
+                      </div>
+                      <img class="img-circle img-responsive img-fluid" text-center src="images/luis-unsplash.jpg"  style="position:absolute;width:100px; max-height:auto; top:20%; left:25%;">
+                    </div>
+                      <!-- <div style="position: relative">
+                        <img class="img-responsive img-fluid" src="images/badge__icon.jpeg" style="width:200px; max-height:auto;">
+                        <img class="img-circle img-responsive img-fluid" text-center src="images/luis-unsplash.jpg"  style="width:100px; max-height:auto; top:-190px; right:-60px;position:relative;">
+                      </div> -->
+                    </div>
+                
+                    <div class="item" style="padding: 30px 15px; font-size:1.8em;">
+
+
+                        <div class="badge__text lead" style="text-align: center;">John Doe</div>
+                        
+                        <div class="badge" style="text-align: center; font-size:.5em; padding:5px;">Sale Rep</div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col col-sm-4 text-center">
+           
+                    <span>
+                        <p class='lead'><span class="title" style="font-size: 1em; font-weight:bold;padding-top:16px"> Position:</span> 4</p>
+                        <!-- <p ><strong class="title"> Position:</strong><span>4</span></p> -->
+                    </span>
+                   
+             <div class="position__img mx-auto"></div>
+            </div>
+            <hr>
+            <div class="col col-sm-4 text-center">
+                <h2 class="page-header">Today's Data:</h2>
+                <p class="lead">Number of routes: :<span></span></p>
+                <p class="lead">Number of Customers:<span></span></p>
+                <p class="lead">Customers served::<span></span></p>
+
+           
+        
+          </div>
+         
+        </div>
+        <hr>
+        <h2 class="main-header page-header" style="text-align: center; font-size: 2.3em; color: #000;">Time</h2>
+       
+        <blockquote class="blockquote">
+        <div class="row my-5 my-row">
+
+<div class="col col-sm-4 font-weight-bold my-col text">
+    <p class="title lead"><strong> Clock In:</strong></p>
+    <p>01/01/2020 12:00AM</p>
+</div>
+<div class="col col-sm-4 font-weight-bold my-col text">
+    <p class="title lead"><strong> Time in Trade: </strong></p>
+    <p class="lead"> 04:13:00</p>
+</div>
+<div class="col col-sm-4 text-nowrap font-weight-bold my-col text">
+    <p class="title lead"><strong> Clock Out: </strong></p>
+    <p class="lead">01/01/2020 12:00AM</p>
+</div>
+<div class="col col-sm-4 font-weight-bold my-col text">
+    <p class="title lead"><strong> Day to Week:</strong></p>
+    <p class="lead">10hours</p>
+</div>
+<div class="col col-sm-4 font-weight-bold my-col text">
+    <p class="title lead"><strong> Week to Month:</strong></p>
+    <p class="lead">24hours</p>
+</div>
+</div>
+        </blockquote>
+        <hr>
+       
+        <div class="row align-items-center my-5">
+            <h2 class="main-header page-header" style="text-align: center; font-size: 2.3em; color: #000;">Sales Target </h2>
+            <div id="chart_div" style="width: 100%; height:150px; padding:20px 20px 50px 20px; display:flex;justify-content:space-evenly">
+        
+         </div>
+         <hr>
+        <div class="row">
+            <div class="container">
+                <h2 class="page-header" style="text-align:center; font-size: 2.3em;  color: #000;">Leaderboard</h2>
+                
+                <div class="center">
+  <div class="top3">
+    <div class="two item">
+      <div class="pos">
+        2
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/men/44.jpg&#39;)"></div>
+      <div class="name">
+        Edgar Soto
+      </div>
+      <div class="score">
+        6453
+      </div>
+    </div>
+    <div class="one item">
+      <div class="pos">
+        1
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/men/31.jpg&#39;)"></div>
+      <div class="name">
+        Clifford James
+      </div>
+      <div class="score">
+        6794
+      </div>
+    </div>
+    <div class="three item">
+      <div class="pos">
+        3
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/91.jpg&#39;)"></div>
+      <div class="name">
+        Nevaeh Silva
+      </div>
+      <div class="score">
+        6034
+      </div>
+    </div>
+  </div>
+  <div class="list">
+    <div class="item">
+      <div class="pos">
+        4
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/men/88.jpg&#39;)"></div>
+      <div class="name">
+        Clayton Watson
+      </div>
+      <div class="score">
+        5980
+      </div>
+    </div>
+    <div class="item">
+      <div class="pos">
+        5
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/29.jpg&#39;)"></div>
+      <div class="name">
+        Debbie Lane
+      </div>
+      <div class="score">
+        5978
+      </div>
+    </div>
+    <div class="item">
+      <div class="pos">
+        6
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/85.jpg&#39;)"></div>
+      <div class="name">
+        Gabriella Steward
+      </div>
+      <div class="score">
+        5845
+      </div>
+    </div>
+    <div class="item">
+      <div class="pos">
+        7
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/39.jpg&#39;)"></div>
+      <div class="name">
+        Nina Perkins
+      </div>
+      <div class="score">
+        5799
+      </div>
+    </div>
+    <div class="item">
+      <div class="pos">
+        8
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/men/77.jpg&#39;)"></div>
+      <div class="name">
+        Dennis Henry
+      </div>
+      <div class="score">
+        5756
+      </div>
+    </div>
+    <div class="item">
+      <div class="pos">
+        9
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/49.jpg&#39;)"></div>
+      <div class="name">
+        Courtney Fuller
+      </div>
+      <div class="score">
+        5713
+      </div>
+    </div>
+    <div class="item">
+      <div class="pos">
+        10
+      </div>
+      <div class="pic" style="background-image: url(&#39;https://randomuser.me/api/portraits/women/30.jpg&#39;)"></div>
+      <div class="name">
+        Joan Wood
+      </div>
+      <div class="score">
+        5674
+      </div>
+    </div>
+  </div>
+</div>
+                <!-- bootstrap card with row name myCarousel as row 1-->
+                <!-- <div class="carousel slide" id="myCarousel">
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <div class="col-xs-2">
+                                <a href="#">
+                                    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20190709143904/391.png"
+                                        class="img-responsive">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-2">
+                                <a href="#">
+                                    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20190709143850/1382.png"
+                                        class="img-responsive">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <div class="col-xs-2">
+                                <a href="#">
+                                    <img src="https://media.geeksforgeeks.org/wp-content/uploads/20190709143855/223-1.png"
+                                        class="img-responsive">
+                                </a>
+                            </div>
+                        </div>
+                    </div> <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <i class="glyphicon glyphicon-chevron-left">
+                        </i>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <i class="glyphicon glyphicon-chevron-right">
+                        </i>
+                    </a>
+
+                </div> -->
+            </div>
+        </div>
+        <div class="table-responsive">
+            <h2 class="page-header">PSKU's</h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Target</th>
+                        <th scope="col">Achieved</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row" class="lead">Hypermatt</th>
+                        <td>11111</td>
+                        <td>10000</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="lead">Supermarket</th>
+                        <td>11111</td>
+                        <td>10000</td>
+                    </tr>
+                    <tr>
+                        <th scope="row" class="lead">Kiosk</th>
+                        <td>11111</td>
+                        <td>10000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="table-responsive">
+            <h2 class="page-header">DSKU's</h2>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Target</th>
+                        <th scope="col">Achieved</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Hypermatt</th>
+                        <td>11111</td>
+                        <td>10000</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Supermarket</th>
+                        <td>11111</td>
+                        <td>10000</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Kiosk</th>
+                        <td>11111</td>
+                        <td>10000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- <div class="row align-items-center my-5">
+            <h2 class="main-header" style="text-align: center; font-size: 2.3em; color: #000;">PSKU </h2>
+            <div class="col col-xs-12 my-col">
+                <div class="progress">
+                    <div class="progress-bar " role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style="width: 71%; background-color:linear-gradient(rgb(255, 255, 255, 0.15));"></div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row align-items-center my-5">
+            <h2 class="main-header" style="text-align: center; font-size: 2.3em; color: #000;">DSKU </h2>
+            <div class="col col-xs-12 my-col">
+                <div class="progress">
+                    <div class="progress-bar " role="progressbar" aria-valuenow="60" aria-valuemin="0"
+                        aria-valuemax="100" style="width: 45%"></div>
+
+                </div>
+
+            </div>
+        </div> -->
+
+
+    </div>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="app.js"></script>
+    <!-- <script type="text/javascript" src="carousel.js"></script> -->
+   
+   
+   
+
+  
+    <!-- Latest compiled JavaScript -->
+    
+
+</body>
+
+</html>
